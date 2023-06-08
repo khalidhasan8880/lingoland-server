beab1e173af5f7e9d52764b370539095136affc0
khalidhasan8880 <khalidhasan8880@gmail.com>
2023-06-09 00:15:52 +0600

    added user role checker

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-require('dotenv').config()
 var jwt = require('jsonwebtoken');
 const stripe = require('stripe')(process.env.PAYMENT_SECRET_KEY)
 const app = express()
@@ -8,16 +8,42 @@ const port = process.env.PORT || 5000
 app.use(express.json())
 app.use(cors())
 
-console.log(process.env.DB_USER);
 
-// create jwt token
+// create jwt token --------------
 app.post('/jwt', (req, res) => {
   const user = req.body
   const token = jwt.sign(user, process.env.SECRET_KEY);
   res.send({ token })
 })
 
-// mongodb
+// verify jwt -------------------
+
+
+
+
+
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization
+  if (!authorization) {
+      return res.status(401).send({ error: true, message: 'unauthorized access' })
+  }
+
+  const token = authorization.split(' ')[1]
+  jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+      if (err) {
+          return res.status(401).send({ error: true, message: 'unauthorized access' })
+      }
+      req.decoded = decoded
+      next()
+  })
+}
+
+
+
+
+
+
+// mongodb ------------------
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sgqndpo.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -37,24 +63,43 @@ async function run() {
     const classCollection = client.db("lingolandDb").collection("classes");
     const userCollection = client.db("lingolandDb").collection("users");
 
-    // insert user
-    app.put('/user/:email', async(req, res)=>{
+    // save user information in database
+    app.put('/users/:email', async (req, res) => {
       const email = req.params.email;
       const user = req.body;
 
-console.log(user);
-console.log(email);
-      const query = {email:email}
-      const options = {upsert:true}
+      const query = { email: email }
+      const options = { upsert: true }
       const updateUser = {
-        $set:user
+        $set: user
       }
 
+      const result = await userCollection.updateOne(query, updateUser, options)
+      res.send(result)
+    })
+    // -----------user----user-----user----user-----user----user-----user----user-----user----user-----user
+    // TODO: MAKE PRIVET
 
-      const result = await userCollection.updateOne(query,updateUser, options)
+    // get all user 
+    app.get('/users', async (req, res) => {
+      const result = await userCollection.find().toArray()
       res.send(result)
     })
 
+    // user role checking
+    app.get('/users/role/:email', verifyJWT,  async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email }
+      console.log('verifyEmail', req?.decoded?.email);
+      console.log('req email', email);
+      if ( req?.decoded?.email !== req?.params?.email) {
+        return res.status(403).send({error:true, message:'forbidden access'})
+      }
+      const user = await userCollection.findOne(query)
+      res.send({role:user?.role})
+
+    })
+    // -user----user-----user----user-----user----user-----user----user-----user----user-----user----user----
 
 
 
